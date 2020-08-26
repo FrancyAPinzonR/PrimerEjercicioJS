50c9038258805798747ef9785ddd982f3dedf3b5
Francy A Pinzon Ramos <francykale@gmail.com>
2020-08-25 23:33:31 -0500

    ajustes al js y css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-var divDatoNombre = document.getElementById("nombre")
+var divDatoNombre = document.getElementById("nombref")
 console.log(divDatoNombre)
-var divDatoEdad = document.getElementById("edad")
+var divDatoEdad = document.getElementById("edadf")
 console.log(divDatoEdad)
-var divDatoCiudad = document.getElementById("ciudad")
+var divDatoCiudad = document.getElementById("ciudadf")
 console.log(divDatoCiudad)
-var divDatoGenero = document.getElementById("genero")
+var divDatoGenero = document.getElementById("generof")
 console.log(divDatoGenero)
+
 
 function Cargardatos() {
     var nombre = prompt('Escriba el nombre del artista')
@@ -17,5 +18,67 @@ function Cargardatos() {
     var genero = prompt('Escriba el genero musical del artista')
     divDatoGenero.innerText = genero
 
+}
+
+var divDatoNombre1 = document.getElementById("nombrej")
+console.log(divDatoNombre1)
+var divDatoEdad1 = document.getElementById("edadj")
+console.log(divDatoEdad1)
+var divDatoCiudad1 = document.getElementById("ciudadj")
+console.log(divDatoCiudad1)
+var divDatoGenero1 = document.getElementById("generoj")
+console.log(divDatoGenero1)
+
+function Cargardatos1() {
+    var nombre = prompt('Escriba el nombre del artista')
+    divDatoNombre1.innerText = nombre
+    var edad = prompt('Escriba la edad del artista')
+    divDatoEdad1.innerText = edad
+    var ciudad = prompt('Escriba la ciudad de origen del artista')
+    divDatoCiudad1.innerText = ciudad
+    var genero = prompt('Escriba el genero musical del artista')
+    divDatoGenero1.innerText = genero
+
+}
+
+var divDatoNombre2 = document.getElementById("nombrer")
+console.log(divDatoNombre2)
+var divDatoEdad2 = document.getElementById("edadr")
+console.log(divDatoEdad2)
+var divDatoCiudad2 = document.getElementById("ciudadr")
+console.log(divDatoCiudad2)
+var divDatoGenero2 = document.getElementById("generor")
+console.log(divDatoGenero2)
+
+function Cargardatos2() {
+    var nombre = prompt('Escriba el nombre del artista')
+    divDatoNombre2.innerText = nombre
+    var edad = prompt('Escriba la edad del artista')
+    divDatoEdad2.innerText = edad
+    var ciudad = prompt('Escriba la ciudad de origen del artista')
+    divDatoCiudad2.innerText = ciudad
+    var genero = prompt('Escriba el genero musical del artista')
+    divDatoGenero2.innerText = genero
+
+}
+
+var divDatoNombre3 = document.getElementById("nombreb")
+console.log(divDatoNombre3)
+var divDatoEdad3 = document.getElementById("edadb")
+console.log(divDatoEdad3)
+var divDatoCiudad3 = document.getElementById("ciudadb")
+console.log(divDatoCiudad3)
+var divDatoGenero3 = document.getElementById("generob")
+console.log(divDatoGenero3)
+
+function Cargardatos3() {
+    var nombre = prompt('Escriba el nombre del artista')
+    divDatoNombre3.innerText = nombre
+    var edad = prompt('Escriba la edad del artista')
+    divDatoEdad3.innerText = edad
+    var ciudad = prompt('Escriba la ciudad de origen del artista')
+    divDatoCiudad3.innerText = ciudad
+    var genero = prompt('Escriba el genero musical del artista')
+    divDatoGenero3.innerText = genero
 
 }
